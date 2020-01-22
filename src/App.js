e71b613df43dc76277cc218684df695107402e1f
AndroidPie <liberasoftware@gmail.com>
2020-01-22 19:33:05 +0100

    implemented actions

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import './App.css';
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {decrement, increment} from "./actions/counterAction";
+import {logging} from "./actions/logginAction";
 
 function App() {
 
   const counter = useSelector(state => state.counterReducer);
   const isLogged = useSelector(state => state.loggedReducer);
+  const dispatch = useDispatch();
 
   return (
     <div className="App">
       <h1>Counter: {counter}</h1>
-      <button>+</button>
-      <button>-</button>
-      {isLogged ? <h3>Valuable information user shouldn't see if not logged in</h3> : ''}
+      <button onClick={() => dispatch(increment(5))}>+</button>
+      <button onClick={()=> dispatch(decrement())}>-</button>
+      <button onClick={()=> dispatch(logging())}>LOGIN</button>
+      {isLogged ? <h3>Valuable information user shouldn't see if not logged in</h3> :
+        <h3>You are not logged in</h3>}
     </div>
   );
 }
---
+++ src/actions/logginAction.js
@@ -0,0 +1,5 @@
+export const logging = () => {
+  return {
+    type: 'SIGN_IN'
+  }
+}
